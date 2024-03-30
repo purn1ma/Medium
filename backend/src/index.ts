5e64c90b63d83d187429da1f7ced15bbb49c5ea9
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2024-03-31 03:01:16 +0530

    authentication added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { authRouter } from "./router/auth"
 import { blogRouter } from "./router/blog"
 
 const app = express()
+app.use(express.json())
 app.use('/api/v1',authRouter)
 app.use('/api/v1/blog',blogRouter)
 app.listen(5000,()=>console.log("server started"))
--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -1,11 +1,67 @@
 import { Router } from "express";
-const router = Router()
-router.post('/signup',(req,res)=>{
-    res.send("signup route")
-})
+import db from "../lib/db";
+import bcrypt from "bcrypt";
+import jwt  from "jsonwebtoken";
 
-router.post('/signin',(req,res)=>{
-    res.send("signin route")
-})
+const router = Router();
+router.post("/signup", async (req, res) => {
+  try {
+    const userDetail = req.body;
+    const userExists = await db.user.findFirst({
+      where: {
+        email: userDetail.email
+      },
+    });
+    if (userExists) {
+      return res.status(409).json({ msg: "user already exist" });
+    }
+    const hashedPassword = await bcrypt.hash(userDetail.password, 10);
+    const user = await db.user.create({
+      data: {
+        name: userDetail.name,
+        email: userDetail.email,
+        password: hashedPassword,
+      },
+    });
+    const token = jwt.sign({id:user.id }, process.env.JWT_SECRET as string)
+    console.log(token)
+    console.log(user)
+    return res.status(201).json({  
+        token, 
+        userId:user.id,
 
-export {router as authRouter}
+    })
+  } catch (err:any) {
+    console.log(err)
+    return res.status(500).json({msg:"something went wrong"})
+  }
+});
+
+router.post("/signin", async(req, res) => {
+    try{
+        const userDetail=req.body;
+        const userExists=await db.user.findFirst({
+            where:{
+                email:userDetail.email
+            }
+        })
+        if(!userExists){
+            return res.status(404).json({msg:"user not found"})
+        }
+        const isPasswordValid=bcrypt.compare(userDetail.password,userExists.password)
+        if(!isPasswordValid){
+            return res.status(403).json({
+                msg:"wrong credentials"
+            })
+        }
+        const token=jwt.sign({id:userExists.id},process.env.JWT_SECRET as string)
+        return res.status(200).json(token)
+    }
+    catch(err:any){
+        console.log(err)
+        return res.status(500).json({msg:"something went wrong"})
+    }
+  
+});
+
+export { router as authRouter };
