aaf75481e8923f29c2adcbb9eda0fd16fa29cddb
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2024-04-08 02:09:56 +0530

    routes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Blog from './pages/Blog'
+import Blogs from './pages/Blogs'
+import Publish from './pages/Publish'
+
+function App() {
+  
+
+  return ( 
+  <Router>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/blog/:id' element={<Blog/>}/>
+      <Route path='/blogs' element={<Blogs/>}/>
+      <Route path='/publish' element={<Publish/>}/>
+      
+
+    </Routes>
+  
+  </Router>)
+}
+
+export default App
